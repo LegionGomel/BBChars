716d44a7267b14bf6400050346b0938e8aaea680
Yuri Ershov <shtorm0606@gmail.com>
2022-01-22 09:45:09 +0300

    Make sure that navigation between views is working. Make some refactoring

--- BBChars/CharactersTableViewController.swift
+++ BBChars/CharactersTableViewController.swift
@@ -7,19 +7,32 @@
 
 import UIKit
 
-class CharactersTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CharactersTableViewController: UIViewController {
+    
+    enum Segues {
+        static let characterDetail = "ToCharacterDetailViewController"
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+extension CharactersTableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
+        //TODO:  need to replace with actual return
+        return 116
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //TODO:  need to replace with actual return
+        UITableViewCell()
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: Segues.characterDetail, sender: self)
     }
 }
